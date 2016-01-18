b3f3861a2ce19cd00afc544c6351286cfc15e6cb
Gurkenschreck <mariogiugno@gmx.de>
2016-01-18 02:57:01 +0100

    Added Sandbox sample application. Just playing around with it

---
+++ Sandbox/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cipha.Security.Cryptography.Symmetric;
+using System.Security.Cryptography;
+using System.IO;
+using System.Diagnostics;
+using System.Threading;
+namespace Sandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericSymmetricCipher<TripleDESCryptoServiceProvider> cipher = new GenericSymmetricCipher<TripleDESCryptoServiceProvider>();
+
+            byte[] key = null, iv = null;
+
+            cipher.EncryptFile("1967.rar", "encrypted.rar", ref key, ref iv);
+
+            cipher.DecryptFile("encrypted.rar", "1111.rar", key, iv);
+
+            Console.WriteLine("fin");
+            Console.ReadKey(true);
+        }
+        public static void Flush()
+        {
+            List<MemoryStream> streams = new List<MemoryStream>(1000);
+            MemoryStream ms = null;
+            for (int i = 0; i < 150; i++)
+            {
+                try
+                {
+                    ms = new MemoryStream();
+                    bool d = true;
+                    while (d)
+                    {
+                        ms.WriteByte(0x00);
+                    }
+                }
+                catch (OutOfMemoryException ex) { }
+                finally
+                {
+                    ms.Close();
+                    streams.Add(ms);
+                    Console.WriteLine(i);
+                }
+            }
+
+            foreach (MemoryStream m in streams)
+            {
+                m.Dispose();
+            }
+            streams.Clear();
+            streams = null;
+        }
+    }
+}
