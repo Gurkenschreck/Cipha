1db86f06caf846e6766ed3ad4d8b1038622dde65
Gurkenschreck <mariogiugno@gmx.de>
2016-01-20 01:21:58 +0100

    Cleared sandbox

--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -14,47 +14,8 @@ namespace Sandbox
     {
         static void Main(string[] args)
         {
-            GenericSymmetricCipher<TripleDESCryptoServiceProvider> cipher = new GenericSymmetricCipher<TripleDESCryptoServiceProvider>();
 
-            byte[] key = null, iv = null;
-
-            cipher.EncryptFile("1967.rar", "encrypted.rar", ref key, ref iv);
-
-            cipher.DecryptFile("encrypted.rar", "1111.rar", key, iv);
-
-            Console.WriteLine("fin");
             Console.ReadKey(true);
-        }
-        public static void Flush()
-        {
-            List<MemoryStream> streams = new List<MemoryStream>(1000);
-            MemoryStream ms = null;
-            for (int i = 0; i < 150; i++)
-            {
-                try
-                {
-                    ms = new MemoryStream();
-                    bool d = true;
-                    while (d)
-                    {
-                        ms.WriteByte(0x00);
-                    }
-                }
-                catch (OutOfMemoryException) { }
-                finally
-                {
-                    ms.Close();
-                    streams.Add(ms);
-                    Console.WriteLine(i);
-                }
-            }
-
-            foreach (MemoryStream m in streams)
-            {
-                m.Dispose();
-            }
-            streams.Clear();
-            streams = null;
         }
     }
 }
