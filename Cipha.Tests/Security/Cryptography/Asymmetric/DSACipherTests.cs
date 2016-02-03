f276a5e24a3277c2df983762d703350dfffa950f
Gurkenschreck <mariogiugno@gmx.de>
2016-02-03 18:49:43 +0100

    Update test

---
+++ Cipha.Tests/Security/Cryptography/Asymmetric/DSACipherTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cipha.Security.Cryptography.Asymmetric;
+using System.Security.Cryptography;
+
+namespace Cipha.Tests.Security.Cryptography.Asymmetric
+{
+    [TestClass]
+    public class DSACipherTest
+    {
+        [TestMethod]
+        public void SignData_SignAndVerifyMessage_Pass()
+        {
+            Random rdm = new Random();
+            byte[] randomMessage = new byte[128];
+            rdm.NextBytes(randomMessage);
+
+            using(var cipher = new DSACipher<DSACryptoServiceProvider>())
+            {
+            }
+        }
+    }
+}
