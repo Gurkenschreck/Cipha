a386a857daf93cabc39647108bfe09cadb947118
Gurkenschreck <mariogiugno@gmx.de>
2016-02-16 01:35:41 +0100

    Add basic hasher, basic tests. More tests to come

---
+++ Cipha.Tests/Security/Cryptography/Hash/HasherTests.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Security.Cryptography;
+using System.Text;
+using System.IO;
+
+namespace Cipha.Tests.Security.Cryptography.Hash
+{
+    [TestClass]
+    public class HasherTests
+    {
+        [TestMethod]
+        public void Instantiate_InstantiateAndDontDispose_Pass()
+        {
+            SHA1Cng refAlgo = new SHA1Cng();
+            using(var digester = new Hasher<SHA1Cng>(refAlgo, false))
+            { }
+            Assert.IsNotNull(refAlgo);
+        }
+        [TestMethod]
+        public void Hash_CompareHasheStringrAndNativeOutput_Pass()
+        {
+            string message = "Hash me";
+            byte[] digesterHash;
+            byte[] nativeHash;
+            using (var digester = new Hasher<SHA1Cng>())
+            {
+                digesterHash = digester.Hash(message);
+                nativeHash = new SHA1Cng()
+                    .ComputeHash(digester.Encoding.GetBytes(message));
+            }
+            CollectionAssert.AreEqual(nativeHash, digesterHash);
+        }
+        [TestMethod]
+        public void Hash_CompareHasherBytesAndNativeOutput_Pass()
+        {
+            string message = "Hash me";
+            byte[] messageData = Encoding.UTF8.GetBytes(message);
+            byte[] digesterHash;
+            byte[] nativeHash;
+            using (var digester = new Hasher<SHA1Cng>())
+            {
+                digesterHash = digester.Hash(messageData);
+                nativeHash = new SHA1Cng().ComputeHash(messageData);
+            }
+            CollectionAssert.AreEqual(nativeHash, digesterHash);
+        }
+        [TestMethod]
+        public void Hash_CompareHasherStreamAndNativeOutput_Pass()
+        {
+            string message = "Hash me";
+            byte[] messageData = Encoding.UTF8.GetBytes(message);
+            byte[] digesterHash;
+            byte[] nativeHash;
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using(StreamWriter sw = new StreamWriter(ms))
+                {
+                    sw.Write(messageData);
+
+                    using (var digester = new Hasher<SHA1Cng>())
+                    {
+                        digesterHash = digester.Hash(ms);
+                        nativeHash = new SHA1Cng().ComputeHash(ms);
+                    }
+                }
+            }
+            CollectionAssert.AreEqual(nativeHash, digesterHash);
+        }
+        [TestMethod]
+        public void HashToString_CompareHasherBytesAndNativeOutput_Pass()
+        {
+            string message = "Hash me";
+            byte[] messageData = Encoding.UTF8.GetBytes(message);
+            string digesterHash;
+            string nativeHash;
+            using (var digester = new Hasher<SHA1Cng>())
+            {
+                digesterHash = digester.HashToString(messageData);
+                nativeHash = Convert.ToBase64String(new SHA1Cng()
+                    .ComputeHash(messageData));
+            }
+            Assert.AreEqual(nativeHash, digesterHash);
+        }
+        [TestMethod]
+        public void HashToString_CompareHasherStringAndNativeOutput_Pass()
+        {
+            string message = "Hash me";
+            string digesterHash;
+            string nativeHash;
+            using (var digester = new Hasher<SHA1Cng>())
+            {
+                digesterHash = digester.HashToString(message);
+                nativeHash = Convert.ToBase64String(new SHA1Cng()
+                    .ComputeHash(digester.Encoding.GetBytes(message)));
+            }
+            Assert.AreEqual(nativeHash, digesterHash);
+        }
+    }
+}
---
+++ Cipha/Security/Cryptography/Hash/Hasher.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cipha.Tests.Security.Cryptography.Hash
+{
+    public class Hasher<T> : IDisposable
+        where T : HashAlgorithm, new()
+    {
+        T algo;
+        bool disposeAlgorithm = true;
+        Encoding encoding = Encoding.UTF8;
+
+        public T Algorithm
+        {
+            get
+            {
+                return algo;
+            }
+            set
+            {
+                algo = value;
+            }
+        }
+        public Encoding Encoding
+        {
+            get { return encoding; }
+            set { encoding = value; }
+        }
+
+        // Decorator
+        public int InputBlockSize
+        {
+            get
+            {
+                return algo.InputBlockSize;
+            }
+        }
+        public int OutputBlockSize
+        {
+            get
+            {
+                return algo.OutputBlockSize;
+            }
+        }
+        public int HashSize
+        {
+            get
+            {
+                return algo.HashSize;
+            }
+        }
+        public byte[] AlgoHash
+        {
+            get
+            {
+                return algo.Hash;
+            }
+        }
+
+        public Hasher()
+        {
+            algo = new T();
+        }
+        public Hasher(T refAlgo, bool disposeAlgorithm = false)
+        {
+            if (refAlgo == null)
+                throw new ArgumentNullException("refAlgo");
+            algo = refAlgo;
+            this.disposeAlgorithm = disposeAlgorithm;
+        }
+
+        public byte[] Hash(Stream dataStream)
+        {
+            return algo.ComputeHash(dataStream);
+        }
+        public byte[] Hash(string stringToHash)
+        {
+            return Hash(encoding.GetBytes(stringToHash), 1);
+        }
+        public byte[] Hash(byte[] dataToHash)
+        {
+            return Hash(dataToHash, 1);
+        }
+        
+        public byte[] Hash(byte[] dataToHash, int offSet, int length)
+        {
+            byte[] wannabeHash = new byte[length];
+            Array.Copy(dataToHash, offSet, wannabeHash, 0, length);
+            return Hash(wannabeHash, 1);
+        }
+        public byte[] Hash(byte[] dataToHash, int rounds)
+        {
+            if (dataToHash == null)
+                throw new ArgumentNullException("dataToHash");
+            if (rounds < 1)
+                throw new ArgumentOutOfRangeException("rounds cannot be < 1");
+
+            byte[] hash = algo.ComputeHash(dataToHash);
+
+            for (int i = 1; i < rounds; i++)
+                hash = algo.ComputeHash(hash);
+
+            return hash;
+        }
+
+        public string HashToString(string stringToHash)
+        {
+            return HashToString(stringToHash, 1);
+        }
+        public string HashToString(string stringToHash, int rounds)
+        {
+            return HashToString(encoding.GetBytes(stringToHash), rounds);
+        }
+        public string HashToString(byte[] dataToHash)
+        {
+            return HashToString(dataToHash, 1);
+        }
+        public string HashToString(byte[] dataToHash, int rounds)
+        {
+            return Convert.ToBase64String(Hash(dataToHash, rounds));
+        }
+
+        ~Hasher()
+        {
+            Dispose(false);
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool isDisposing)
+        {
+            if(isDisposing)
+            {
+                if(disposeAlgorithm)
+                    algo.Dispose();
+                algo = null;
+            }
+        }
+    }
+}
